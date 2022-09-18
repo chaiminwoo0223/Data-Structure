9dc2d6d24538666fb3eefe941a3e73dfdeba7dea
chaiminwoo0223 <112475136+chaiminwoo0223@users.noreply.github.com>
2022-09-18 21:51:26 +0900

    Add files via upload

--- lab5_1.java
+++ lab5_1.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class lab5_1 {
 
 	public static void main(String[] args) {
-		System.out.println("lab5_1 : ÃÖ¹Î¿ì");
+		System.out.println("lab5_1 : ìµœë¯¼ìš°");
 
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		// 1~10
@@ -13,12 +13,12 @@ public class lab5_1 {
 			list.add(i+1);
 		}
 		System.out.println(list);
-		// »ğÀÔ
+		// ì‚½ì…
 		list.add(0,99);
 		list.add(3,999);
 		list.add(12,999);
 		System.out.println(list);
-		// »èÁ¦
+		// ì‚­ì œ
 		System.out.println(list.remove(12));
 		System.out.println(list.remove(4));
 		System.out.println(list.remove(0));
