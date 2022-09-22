b0a8ce19dff31b6ccc2d6be804cdbd3b6ee161d3
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2022-09-22 14:36:18 +0900

    Update hw5_1.java

--- hw5_1.java
+++ hw5_1.java
@@ -3,7 +3,8 @@ package hw5_1;
 public class hw5_1 {
 
 	public static void main(String[] args) {
-		System.out.println("lab5_1 : 최민우");
+		System.out.println("hw5_1 : 최민우");
+		
 		//MyArrayList형 리스트 객체 list를 생성
 		MyArrayList list = new MyArrayList(100);
 		// 1~10
@@ -18,21 +19,39 @@ public class hw5_1 {
 		System.out.println(list);
 	}
 	//배열을 이용하여 정수 리스트를 구현하는 클래스
-	public class MyArrayList{
-		private int[] array; //원소를 저장할 배열
-		private int numItems; //배열에 저장된 원소 수(리스트 길이)
-		public static final int DEFAULT_CAPACITY = 100; //배열의 기본 용량
-		//생성자1
-		public MyArrayList() {
-			array = new int[DEFAULT_CAPACITY];
-			numItems = 0;
+	
+	}
+class MyArrayList{
+	private int[] array; //원소를 저장할 배열
+	private int numItems; //배열에 저장된 원소 수(리스트 길이)
+	public static final int DEFAULT_CAPACITY = 100; //배열의 기본 용량
+	//생성자1
+	public MyArrayList() {
+		array = new int[100];
+		numItems = 0;
+	}
+	//생성자2
+	public MyArrayList(int capacity) {
+		array = new int[capacity];
+		numItems = 0;
+	}
+	//삽입
+	public void add(int item) {
+		if(numItems < array.length)
+			array[numItems++] = item;
+	}
+	public void add(int index,int item) {
+		array[index] = item;
+	}
+	@Override
+	public String toString() {
+		String result = "";
+		for(int i=0;i<array.length;i++) {
+			result += array[i] + " ";
 		}
-		//생성자2
-		public MyArrayList(int capacity) {
-			array = new int[capacity];
-			numItems = 0;
-		}
-		//삽입
+		return result;
+	}
+}
 		public void add(int item) {
 			if(numItems < array.length)
 				array[numItems++] = item;
