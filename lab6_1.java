406a9a59d73750692ca7ea8cbd707fa0c713529d
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2022-09-30 17:45:14 +0900

    Update lab6_1.java

--- lab6_1.java
+++ lab6_1.java
@@ -6,20 +6,20 @@ public class lab6_1 {
 	public static void main(String[] args) {
 		System.out.println("lab6_1 : 최민우");
 		
-		// 정수원소를저장할LinkedList형리스트객체list를생성
+		// LinkedList형 리스트 객체 list를생성
 		LinkedList<Integer> list = new LinkedList<Integer>();
 		
-		//list에 2가 존재하는지 여부 출력
+		// list에 2가 존재하는지 여부 출력
 		System.out.println(list.contains(2));
 		
-		// list의맨앞에1, 2를, 맨뒤에3, 4를차례대로삽입한후,리스트출력
+		// 삽입
 		list.addFirst(1); 
 		list.addFirst(2); 
 		list.addLast(3);
 		list.addLast(4);
 		System.out.println(list);
 		
-		// list에2, 4, 6이존재하는지여부출력
+		// list에 값이 존재하는지 여부 출력
 		System.out.println(list.contains(2));
 		System.out.println(list.contains(4));
 		System.out.println(list.contains(6));
@@ -27,11 +27,9 @@ public class lab6_1 {
 		//list 길이(크기) 출력
 		System.out.println(list.size());
 		
-		// list의맨앞원소, 맨뒤원소를삭제한후, 두원소의합출력,리스트출력
-		System.out.println(list.removeFirst() + list.removeLast());
-		System.out.println(list);
-		// list의맨앞원소, 맨뒤원소를삭제한후, 두원소의합출력,리스트출력
-		System.out.println(list.removeFirst() + list.removeLast());
-		System.out.println(list);
+		System.out.println(list.removeFirst() + list.removeLast()); // list의 맨앞원소, 맨뒤원소를 삭제한 후, 합 출력
+		System.out.println(list); // 리스트 출력
+		System.out.println(list.removeFirst() + list.removeLast()); // list의 맨앞원소, 맨뒤원소를 삭제한 후, 합 출력
+		System.out.println(list); // 리스트 출력 // 리스트 출력
 	}
 }
