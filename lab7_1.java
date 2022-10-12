25602ef9068d76713379bfa94a33d95575f9d7b6
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2022-10-12 18:49:09 +0900

    Update lab7_1.java

--- lab7_1.java
+++ lab7_1.java
@@ -9,7 +9,7 @@ public class lab7_1 {
 		System.out.println("lab7_1 : 최민우");
 		// 문자 스택 객체 생성
 		Stack<Character> stack = new Stack<Character>();
-		// 사용자로부터 한 의 문장열을 입력받음
+		// 사용자로부터 한 줄의 문장열을 입력받음
 		Scanner input = new Scanner(System.in);
 		String line = input.nextLine();
 		// 문자열의 문자를 하나씩 스택에 삽입
